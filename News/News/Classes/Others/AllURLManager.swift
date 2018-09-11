cb8e9c9f32cee4f592627d336130d8e43787d90f
InnoClan <innoclan@InnoClandeiMac.local>
2018-09-11 12:09:14 +0800

    first commit

---
+++ News/News/Classes/Others/AllURLManager.swift
@@ -0,0 +1,22 @@
+//
+//  AllURLManager.swift
+//  News
+//
+//  Created by Lee on 2018/9/11.
+//  Copyright © 2018年 hrscy. All rights reserved.
+//
+
+import UIKit
+
+let FULI_BASE_URL = "http://backstage.mccoco.com"
+//对应接口名
+/*
+ **全平台内容分类接口接口
+ */
+let HOME_TITLE_API = "/api/api.server.get_index_tag.php"
+
+/*
+ **获取内容列表接口
+ */
+let GET_CONTENT_API = "/api/api.server.get_index_tag.php"
+
